ebc5b44fad9fe6259bc81b7fca886a46db09d101
Luthfi Abid <luthfiabid27@gmail.com>
2019-05-26 16:50:17 +0700

    Tambahan kolom transaksi

--- database/migrations/2019_05_17_035222_create_transaction_table.php
+++ database/migrations/2019_05_17_035222_create_transaction_table.php
@@ -20,6 +20,8 @@ class CreateTransactionTable extends Migration
             $table->integer('id_admin')->unsigned();
             $table->foreign('id_admin')->references('id_admin')->on('admin')->onDelete('cascade');
             $table->string('total_price');
+            $table->integer('isdone');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
