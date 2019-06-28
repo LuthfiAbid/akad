91b0eeecaffd94bdf9d732ad72de3b60faa9e709
annas9029 <annasprastyo@gmail.com>
2019-06-28 17:02:29 +0700

    tambahan category

--- app/Http/Controllers/TrCategoryController.php
+++ app/Http/Controllers/TrCategoryController.php
@@ -17,13 +17,11 @@ class TrCategoryController extends Controller
     {
 
         if(!Session::get('login_buyer')){
-            return view('buyer.content.categories');
-        }else{
-            $data = DB::table('goods')
+        $goods['all'] = DB::table('goods')
             ->join('categories','categories.id_category','goods.id_category')
             ->select('goods.*','categories.*')
-            ->get();
-$detail_transaction = DB::table('transaction')
+            ->paginate(6);
+        $detail_transaction = DB::table('transaction')
             ->join('detail_transaction','transaction.id_transaction','detail_transaction.id_transaction')
             ->join('goods','detail_transaction.id_goods','goods.id_goods')
             ->join('categories','categories.id_category','goods.id_category')
@@ -31,7 +29,37 @@ $detail_transaction = DB::table('transaction')
             ->where('id_buyer',"=", Session::get('id_buyer'))
             ->where('isdone', "=", "0")
             ->get();
-return view('buyer.content.categories', compact('detail_transaction'));
+        $count_data_goods = DB::table('goods')
+            ->count();
+        $goods_w_ts = DB::table('goods')
+            ->join('categories','categories.id_category','goods.id_category')
+            ->select('goods.*','categories.category_name as cat_name')
+            ->orderBy('id_goods','DESC')
+            ->limit(3)
+            ->get();
+        return view('buyer.content.categories',$goods, compact('detail_transaction','count_data_goods','goods_w_ts'));
+        }else{
+        $goods['all'] = DB::table('goods')
+            ->join('categories','categories.id_category','goods.id_category')
+            ->select('goods.*','categories.*')
+            ->paginate(6);
+        $detail_transaction = DB::table('transaction')
+            ->join('detail_transaction','transaction.id_transaction','detail_transaction.id_transaction')
+            ->join('goods','detail_transaction.id_goods','goods.id_goods')
+            ->join('categories','categories.id_category','goods.id_category')
+            ->select('goods.*','detail_transaction.*','transaction.*','categories.category_name as cat_name')
+            ->where('id_buyer',"=", Session::get('id_buyer'))
+            ->where('isdone', "=", "0")
+            ->get();
+        $get_count = DB::table('goods')
+            ->count();
+        $goods_w_ts = DB::table('goods')
+            ->join('categories','categories.id_category','goods.id_category')
+            ->select('goods.*','categories.category_name as cat_name')
+            ->orderBy('id_goods','DESC')
+            ->limit(3)
+            ->get();
+            return view('buyer.content.categories',$goods, compact('detail_transaction','count_data_goods','goods_w_ts'));
         }
     }
 
